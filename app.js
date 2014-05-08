39a2d65e404dd68945f69b4a345b0d9118b8a2c9
Conner Jevning <cjevning@stanford.edu>
2014-05-07 22:40:18 -0700

    matches

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ if ('development' == app.get('env')) {
 // Webpages
 app.get('/', login.viewLogin);
 app.get('/match/:uid', match.match);
+app.post('/match/:uid1/:uid2/:recommender', match.addTo)
 app.get('/home',home.viewHome);
 app.get('/requirement/:requirement',requirement.viewRequirementAlternate);
 app.get('/requirement/:requirement/alternate',requirement.viewRequirementAlternate);
--- routes/match.js
+++ routes/match.js
@@ -1,5 +1,57 @@
+var models = require('../models');
+
 exports.match = function(req,res)
 {
 	req.session.uid = req.params.uid;
 	res.render('match');
+}
+
+exports.addTo = function(req, res) {
+	var uid1 = req.params.uid1;
+	var uid2 = req.params.uid2;
+	var rec = req.params.recommender;
+	var l = models.Match.find({"uid1": uid1, "uid2": uid2}).exec(addToArray);
+	function addToArray(err, toAdd) {
+		if (toAdd[0]) {
+			var recs = toAdd[0].recommenders;
+			var oldNum = toAdd[0].numRecs;
+			toAdd[0].recommenders = recs + "," + rec;
+			toAdd[0].numRecs = oldNum++;
+			toAdd[0].save(afterS);
+		    function afterS (err, t) {
+		    	if(err) console.log(err);
+		    	res.send();
+		    }
+		}
+		else {
+			var m = models.Match.find({"uid1": uid2, "uid2": uid1}).exec(addToArray2);
+			function addToArray2(err, toAdd2) {
+				if (toAdd2[0]) {
+					var recs = toAdd2[0].recommenders;
+					var oldNum = toAdd2[0].numRecs;
+					toAdd2[0].recommenders = recs + "," + rec;
+					toAdd2[0].numRecs = oldNum++;
+					toAdd2[0].save(afterS);
+				    function afterS (err, t) {
+				    	if(err) console.log(err);
+				    	res.send();
+				    }
+				}
+				else {
+					var newMatch = new models.Match({
+				    "uid1": uid1,
+				    "uid2": uid2,
+				    "recommenders": rec,
+				    "numRecs": 1,
+					});
+
+					newMatch.save(afterSave);
+					function afterSave(err, resu) {				    
+					    if(err) console.log(err);
+					    res.send();
+					}
+				}
+			}
+		}
+	}
 }
